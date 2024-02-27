f35dc042574c008d3bd9cac8ad34bc2b5a46fc7a
dlehdg <ldg2409@naver.com>
2024-02-27 23:20:26 +0900

    [✨Feat] : Preparing 컴포넌트, PreparingItem 컴포넌트 마크업 완료 및 HeaderBar 수정 1. 아직 미개발인 페이지를 일때 보여주는 Preparing 컴포넌트를 생성 2. PreparingItem 컴포넌트를 통해 컴포넌트 쪼개기 3. 기존 HeaderBar의 문제점 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import HeaderBar from './components/HeaderBar/HeaderBar';
+import Preparing from './pages/Preparing/Preparing';
 
 function App() {
   return (
     <div className="App">
-      <HeaderBar name={'뒤로가기'} showHomeBtn={true} />
-      {/* <img src="/images/login/test3.svg" alt="테스트" /> */}
+      <Preparing />
     </div>
   );
 }
--- src/components/HeaderBar/HeaderBar.jsx
+++ src/components/HeaderBar/HeaderBar.jsx
@@ -8,21 +8,23 @@ function HeaderBar(props) {
   return (
     <div>
       <div className="justify-center">
-        <div className="flex min-w-375pxr max-w-680pxr items-center justify-around py-4">
+        <div className="flex min-w-375pxr max-w-680pxr items-center justify-between py-4">
           <button type="button" className="gap-10pxr" onClick={handleCheck}>
             <img src="/images/login/arrow.svg" alt="뒤로가기" />
           </button>
 
-          <span className="text-black">{props.name}</span>
+          <span className=" text-black">{props.name}</span>
 
-          {props.showHomeBtn && (
-            <button type="button" className="gap-10pxr" onClick={handleCheck}>
+          {props.showHomeBtn ? (
+            <button type="button" className="" onClick={handleCheck}>
               <img
                 className="justify-center"
                 src="/images/login/HomeIcon.svg"
                 alt="홈으로 가기"
               />
             </button>
+          ) : (
+            <div className="h-22pxr w-22pxr"></div>
           )}
         </div>
       </div>
