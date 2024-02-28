892ebdfc6cdcf7ca8b070c9bc3c7388065392064
junghyunlee0922 <self21c123@naver.com>
2024-02-28 15:55:05 +0900

    [✅Test]: 체크박스 기능구현 하는 도중 test폴더에 옮김. 1. 체크박스 기능구현 하다가 너무 어려워서 일단 하던거 test로 옮김 2. App폴더에 체크박스 폴더 경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import CheckBox from './components/atoms/index';
+import CheckBox from './components/atoms/inputCheckBox';
+// import CheckBox from './components/atoms/test';
 import HeaderBar from './components/HeaderBar/HeaderBar';
 import Preparing from './pages/Preparing/Preparing';
 
 function App() {
-  return (
-    <div className="App">
-      <Preparing />
-      <CheckBox />
-    </div>
-  );
+  return <div className="App">
+    
+  </div>;
 }
 
 export default App;
