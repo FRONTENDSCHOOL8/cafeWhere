1f2260c352b79f5937722e8168ccfab35f91f1a3
bambiru <dj70083798@gmail.com>
2024-02-29 00:27:47 +0900

    [✨Feat]: Category 컴포넌트 구현
    - fetch를 통해 카테고리 DB에 있는 값을 가져왔음
    - 폰트 사이즈가 너무 작아서 10px에서 12px로 변경
    - 디테일한 작업은 다시 수정해야 할 듯 합니다(라우터 등)

--- src/components/atoms/index.jsx
+++ src/components/atoms/index.jsx
@@ -1,4 +1,8 @@
+/* 로그인/회원가입 */
 export { default as PasswordIcon } from './PasswordIcon/PasswordIcon';
 export { default as UserInfoLabel } from './UserInfoLabel/UserInfoLabel';
 export { default as UserInfoInput } from './UserInfoInput/UserInfoInput';
 export { default as ErrorMessage } from './ErrorMessage/ErrorMessage';
+
+/* 메인 카테고리 */
+export { default as CategoryItem } from './CategoryItem/CategoryItem';
