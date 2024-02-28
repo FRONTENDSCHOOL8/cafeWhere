5d48cfb6486d078b161b1e6ced0b13d13d022996
junghyunlee0922 <self21c123@naver.com>
2024-02-29 04:15:40 +0900

    [🚚Rename]: 체크박스 svg 경로 수정 1. svg 바뀐 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import CheckBox from './components/atoms/CheckBox/JoinInputCheckBox';
-// import CheckBox from './components/atoms/CheckBox/Test';
+// import CheckBox from './components/atoms/CheckBox/LastCheckBox';
 import HeaderBar from './components/HeaderBar/HeaderBar';
 import Preparing from './pages/Preparing/Preparing';
 import { LoginInput } from './components/organisms';
 
 function App() {
-  return <div>
-
-  </div>;
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
 export default App;
