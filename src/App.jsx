9ef71d9b309379d651073ff13b489a45670e749f
junghyunlee0922 <self21c123@naver.com>
2024-03-02 21:06:39 +0900

    [🚚Rename]: 회원가입 페이지 체크박스 파일 폴더 이동 1. 파일 위치 바꾼거 경로 이동. 2. 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import CheckBox from './components/atoms/CheckBox/JoinInputCheckBox';
-// import CheckBox from './components/atoms/CheckBox/LastCheckBox';
-import HeaderBar from './components/HeaderBar/HeaderBar';
+import CheckBox from './components/organisms/CheckBox/JoinInputCheckBox';
+// import CheckBox from './components/atoms/CheckBox/LastCheckBox.jsx';
+import HeaderBar from './components/atoms/HeaderBar/HeaderBar';
+import HeaderSwiper from './components/HeaderSwiper/HeaderSwiper';
 import Preparing from './pages/Preparing/Preparing';
 import { LoginInput } from './components/organisms';
 import MainHeader from './components/atoms/MainHeader/MainHeader';
 import TabBar from './components/atoms/TabBar/TabBar';
+import CategoryCafeList from './components/SwiperCafeList/CategoryCafeList';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <CategoryCafeList />
+    </div>
+  );
 }
 
 export default App;
