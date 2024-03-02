c58fd36e5944548faa210710d4b63dc2bbe5bfc1
dlehdg <ldg2409@naver.com>
2024-03-02 21:42:18 +0900

    [✨Feat] : SelectLoginPage 컴포넌트와 컴포넌트들 라우터 구현 1. 페이지 폴더에 있는 컴포넌트를 통해 라우터 기능 구현 2. SelectLoginPage 컴포넌트 구현

--- src/pages/RootLayout/RootLayout.jsx
+++ src/pages/RootLayout/RootLayout.jsx
@@ -1,3 +1,4 @@
+import { TabBar } from '@/components/atoms';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
@@ -5,6 +6,7 @@ function RootLayout() {
   return (
     <div>
       <Outlet />
+      <TabBar />
     </div>
   );
 }
--- src/routes/navigation.jsx
+++ src/routes/navigation.jsx
@@ -2,11 +2,12 @@ import HeaderSwiper from '@/components/HeaderSwiper/HeaderSwiper';
 import { MainHeader } from '@/components/atoms';
 import JoinPage from '@/pages/JoinPage/JoinPage';
 import LoginPage from '@/pages/LoginPage/LoginPage';
+import SelectLoginPage from '@/pages/SelectLoginPage/SelectLoginPage';
 
 const navigationItems = [
   {
     id: 'login',
-    path: '/',
+    path: '/login',
     text: '로그인 페이지',
     element: <LoginPage />,
   },
@@ -21,6 +22,12 @@ const navigationItems = [
     path: '/test',
     text: '테스트용 탭바',
     element: <HeaderSwiper />,
+  },
+  {
+    id: 'selectlogin',
+    path: '/',
+    text: '이메일 로그인과 소셜 로그인',
+    element: <SelectLoginPage />,
   },
 ];
 
