2678a37123016d898b3977c33dd5f9863c25860d
mondimhaus <mondimhaus@naver.com>
2024-03-03 23:12:29 +0900

    [🎨Modify]: LoginButton CSS 수정
    1. tailwind.config 파일 컬러 코드로 수정
    2. mx-20pxr -> mx-5 수정

--- src/components/atoms/LoginButton/LoginButton.jsx
+++ src/components/atoms/LoginButton/LoginButton.jsx
@@ -1,15 +1,15 @@
 function LoginButton() {
   return (
-    <div>
+    <>
       <div className="flex items-center justify-center">
         <button
           type="button"
-          className="text-4 mx-20pxr h-12 w-full rounded-xl bg-[#654E3C] px-4 py-3 text-center text-white"
+          className="text-4 mx-5 h-12 w-full rounded-xl bg-primary-colors px-4 py-3 text-center text-white"
         >
           로그인
         </button>
       </div>
-    </div>
+    </>
   );
 }
 
