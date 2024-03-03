9bd12ba46e3845d9cb972313e4d714b898f084d3
mondimhaus <mondimhaus@naver.com>
2024-03-03 23:14:22 +0900

    [🎨Modify]: SignUpLink CSS 수정
    1. tailwind config 파일 컬러코드로 수정

--- src/components/atoms/SIgnUpLink/SignUpLink.jsx
+++ src/components/atoms/SIgnUpLink/SignUpLink.jsx
@@ -2,7 +2,7 @@ function SignUpLink() {
   return (
     <div className="flex justify-center gap-2 pt-2 text-14pxr">
       <span>계정이 없으신가요?</span>
-      <span className="text-[#654E3C]">회원가입</span>
+      <span className="text-primary-colors">회원가입</span>
     </div>
   );
 }
