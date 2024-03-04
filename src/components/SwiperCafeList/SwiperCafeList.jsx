75dadf196981e6f2b4ff13c346ada2688d590d3c
junghyunlee0922 <self21c123@naver.com>
2024-03-04 10:59:41 +0900

    [✨Feat]: 메인페이지 카테고리 스와이퍼 구현 [🔧Add]: gitignore에 App.jsx추가 1. 메인페이지 카테고리 스와이퍼 구현 2. gitignore에 App.jsx추가     a. gitignore에 추가후 App.jsx 커밋에 안뜨게 수정.

--- src/components/SwiperCafeList/SwiperCafeList.jsx
+++ src/components/SwiperCafeList/SwiperCafeList.jsx
@@ -1,30 +1,103 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
+import { Navigation, FreeMode, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/free-mode';
+import CategoryCafeList from './CategoryCafeList';
 
-import './styles.css';
-
-// import required modules
-import { Navigation } from 'swiper/modules';
-
-export default function App() {
+function SwiperCafeList() {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+      <Swiper
+        className="mySwiper min-w-375pxr max-w-680pxr"
+        slidesPerView={1.5}
+        breakpoints={{
+          375: {
+            slidesPerView: 1.5,
+            spaceBetween: 16,
+          },
+          425: {
+            slidesPerView: 2,
+            spaceBetween: 16,
+          },
+          680: {
+            slidesPerView: 3,
+            spaceBetween: 16,
+          },
+        }}
+        navigation={true}
+        modules={[Navigation, FreeMode, Pagination]}
+      >
+        <SwiperSlide>
+          <CategoryCafeList />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CategoryCafeList />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CategoryCafeList />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CategoryCafeList />
+        </SwiperSlide>
       </Swiper>
     </>
   );
 }
+
+export default SwiperCafeList;
+
+// import React, { useRef, useState } from 'react';
+// // Import Swiper React components
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import CategoryCafeList from './CategoryCafeList';
+
+// // Import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+
+// // import required modules
+// import { Pagination } from 'swiper/modules';
+
+// export default function App() {
+//   return (
+//     <>
+//       <Swiper
+//         slidesPerView={1}
+//         spaceBetween={10}
+//         pagination={{
+//           clickable: true,
+//         }}
+//         breakpoints={{
+//           640: {
+//             slidesPerView: 2,
+//             spaceBetween: 20,
+//           },
+//           768: {
+//             slidesPerView: 4,
+//             spaceBetween: 40,
+//           },
+//           1024: {
+//             slidesPerView: 5,
+//             spaceBetween: 50,
+//           },
+//         }}
+//         modules={[Pagination]}
+//         className="mySwiper"
+//       >
+//         <SwiperSlide>
+//           <CategoryCafeList />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <CategoryCafeList />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <CategoryCafeList />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <CategoryCafeList />
+//         </SwiperSlide>
+//       </Swiper>
+//     </>
+//   );
+// }
