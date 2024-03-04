ed55c5c29492cfe6617948465132128408e00acd
mondimhaus <63273344+mondimhaus@users.noreply.github.com>
2024-03-04 13:36:22 +0900

    Footer, Hashtag 컴포넌트 구현 & LoginButton, SignUpLink CSS 수정 (#28)
    * [🎨Modify]: LoginButton CSS 수정
    
    1. tailwind.config 파일 컬러 코드로 수정
    2. mx-20pxr -> mx-5 수정
    
    * [🎨Modify]: SignUpLink CSS 수정
    
    1. tailwind config 파일 컬러코드로 수정
    
    * [✨Feat]: Footer 마크업 구현
    
    1. 푸터 마크업 구현
    2. Link 태그 - 페이지 이동될 수 있도록 구현 (추후 상위 컴포넌트에 라우터 태그로 감싸기)
    
    * [✨Feat]: Hashtag 마크업, 기능 구현
    
    1. Hashtag 마크업 구현
    2. icon, keyword, isClickable 속성을 받아옴
    3. 클릭 이벤트 - 특정 페이지(검색)에서만 클릭할 수 있도록 제어

--- src/components/atoms/SIgnUpLink/SignUpLink.jsx
+++ src/components/atoms/SIgnUpLink/SignUpLink.jsx
@@ -10,7 +10,7 @@ function SignUpLink() {
   return (
     <div className="flex justify-center gap-2 pt-2 text-14pxr">
       <span>계정이 없으신가요?</span>
-      <span className="cursor-pointer text-[#654E3C]" onClick={handleJoin}>
+      <span className="cursor-pointer text-primary-colors" onClick={handleJoin}>
         회원가입
       </span>
     </div>
