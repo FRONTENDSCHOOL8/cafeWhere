86a465e3c12657ec9ee0a6f5ceee58dbe603e35c
dlehdg <ldg2409@naver.com>
2024-03-04 16:08:47 +0900

    [✨Feat] : 로그인 기능 구현 1. 이메일 로그인 구현 2. 로그인 정보 sessionStorage에 저장 3. 컴포넌트 일부 수정

--- src/components/atoms/MainHeader/MainHeader.jsx
+++ src/components/atoms/MainHeader/MainHeader.jsx
@@ -5,15 +5,15 @@ function MainHeader() {
     <div>
       <div className="flex flex-col ">
         <div className="ml-5 mr-5 mt-5 flex min-w-375pxr max-w-680pxr items-center justify-between ">
-          <button type="button" className="gap-10pxr">
+          <button type="button" className="">
             <img src="/images/main/header/mapLogo.svg" alt="지도로 이동" />
           </button>
 
-          <button type="button" className="gap-10pxr">
+          <button type="button" className="">
             <img src="/images/main/header/mainLogo.svg" alt="메인 로고" />
           </button>
 
-          <button type="button" className="gap-10pxr">
+          <button type="button" className="">
             <img
               src="/images/main/header/searchIcon.svg"
               alt="검색창으로 이동"
