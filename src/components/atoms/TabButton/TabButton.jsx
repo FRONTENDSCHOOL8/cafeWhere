a72ae3edb9df6e1c41a3133e8380e2ca4af4b107
mondimhaus <mondimhaus@naver.com>
2024-03-06 14:15:21 +0900

    [♻️Refactor]: TabButton 스타일 코드 리팩토링 1. border-r, border-l 을 border-x로 수정

--- src/components/atoms/TabButton/TabButton.jsx
+++ src/components/atoms/TabButton/TabButton.jsx
@@ -1,7 +1,7 @@
 function TabButton({ active, onClick, children }) {
   return (
     <button
-      className={`w-full rounded-t-lg border border-l border-r border-t border-[#999] bg-white px-4 py-2 text-center leading-5
+      className={`w-full rounded-t-lg border border-x border-t border-[#999] bg-white px-4 py-2 text-center leading-5
         ${active ? 'border-b-0' : ''}`}
       onClick={onClick}
     >
