8539ba269601116afcf67d8b00e18e07140df28d
mondimhaus <mondimhaus@naver.com>
2024-03-06 11:50:54 +0900

    [📑Docs]: 머지 주석 삭제 1. 머지할 때 생성된 주석을 삭제했습니다.

--- src/components/atoms/LoginButton/LoginButton.jsx
+++ src/components/atoms/LoginButton/LoginButton.jsx
@@ -3,11 +3,7 @@ function LoginButton() {
     <>
       <div className="flex items-center justify-center">
         <button
-<<<<<<< HEAD
-          type="button"
-=======
           type="submit"
->>>>>>> develop
           className="text-4 mx-5 h-12 w-full rounded-xl bg-primary-colors px-4 py-3 text-center text-white"
         >
           로그인
