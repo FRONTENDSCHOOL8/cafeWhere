7d2c12fc22ba6093aa36aadbe0d540de994c90de
BAMBI <116716953+Bambiru@users.noreply.github.com>
2024-03-07 17:31:28 +0900

    Feature/detail[♻️Refactor]: zustand로 전역 상태관리로 변경 및 tanstackQuery 적용중 (#48)
    * [🔧Add]: 아토믹 컴포넌트 폴더 구성 추가
    
    아토믹디자인 컴포넌트 분리 폴더 구성
    
    * [🎨Modify]: sciprt와 meta태그 위치 변경
    
    meta태그들을 위쪽에 몰아넣고, script와 title을 아래로 배치했습니다.
    
    * [🚀Deploy]: 중간 배포 테스트 - 1
    
    - 배경 색 및 일부 디자인 수정
    - 현재까지 완성된 페이지 중간 배포중입니다.
    
    * [💄Design]: Category 디자인 변경 및 메인 추가
    
    Category 8개로 줄어서 사이즈 조정 및 위치 조절,
    메인에 추가해서 넣어놨습니다.
    
    * [💄Design]: 디테일 수정
    
    - Wish : 사이즈 변경을 위한 파라미터 추가
    - TabButton : padding 수정 및 rounded 수정
    - Hashtag : click props가 있는 경우에만 click되도록 변경
    - CafeInfoTab : justify-center 제거
    
    * [✨Feat]: 상세페이지 정보 탭 구현
    
    - 상세페이지 정보 가져오기
    - 맵은 살짝 수정이 필요함
    
    * [✨Feat]: 카테고리 별 카페리스트
    
    지역 상관 없이 카테고리 별 리스트 나오게 만들었습니다
    추후에 지역별로 나오게 만들예정
    
    * [🐛Fix]: 비동기 useEffect 수정
    
    useEffect 안에 비동기 수정
    
    * [🐛Fix]: Category 오류 수정
    
    useEffect, useState 관련 수정
    
    * [🏛️Build]: simple-zustand-devtools 설치
    
    zustand 관련 개발 패키지 설치
    
    * [🎨Modify]: zustand로 region 상태 관리
    
    region을 전역에서 상태관리 할 수 있도록 변경
    추가로 tabStore도 index에 넣어서 경로 수정하였습니다
    
    * [🚚Rename]: useTabStore import 경로 변경
    
    index폴더를 만들어서 그 안에다가 store들을 관리하고 있습니다
    
    * [♻️Refactor]: pages폴더 정리 및 index파일 생성
    
    import라인이 너무 지저분해서 정리 좀 했습니다
    
    * [♻️Refactor]: useState -> 전역 상태관리로 변경
    
    useState로 관리되던 상태들을 전역으로 변경하고 있습니다
    
    * [♻️Refactor]: MainPage 상태관리 변경 및 tanStackQuery 적용중
    
    아직 100% 진행은 안된상태입니다
    
    * [🐛Fix]: reviewLoader 추가
    
    삭제되었던 reviewLoader 추가

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+export { default as MyPage } from './MyPage/MyPage';
+export { default as JoinPage } from './JoinPage/JoinPage';
+export { default as MainPage } from './MainPage/MainPage';
+export { default as LoginPage } from './LoginPage/LoginPage';
+export { default as Preparing } from './Preparing/Preparing';
+export { default as RootLayout } from './RootLayout/RootLayout';
+export { default as DetailPage } from './DetailPage/DetailPage';
+export { default as CafeListPage } from './CafeListPage/CafeListPage';
+export { default as AllHashtagList } from './AllHashtagList/AllHashtagList';
+export { default as SelectLoginPage } from './SelectLoginPage/SelectLoginPage';
+export { default as SearchRegionList } from './SearchRegionList/SearchRegionList';
