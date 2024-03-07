353f539805c9cf15c46c560d42b22db272207007
bambiru <dj70083798@gmail.com>
2024-03-07 10:33:15 +0900

    [🐛Fix]: Category 오류 수정
    useEffect, useState 관련 수정

--- src/components/organisms/Category/Category.jsx
+++ src/components/organisms/Category/Category.jsx
@@ -1,29 +1,37 @@
+import { useState, useEffect } from 'react';
 import { CategoryItem } from '@/components/atoms';
-import { useEffect } from 'react';
 
 const CATEGORY_URL = `${import.meta.env.VITE_PB_URL}/api/collections/category/records`;
 
-useEffect(() => {
-  const getData = async () => {
-    const response = await fetch(CATEGORY_URL);
-    const data = await response.json();
-
-    return data.items;
-  };
-  getData();
-}, []);
-
 function Category() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(CATEGORY_URL);
+        const data = await response.json();
+        setCategories(data.items); // 데이터 가져온 후 state 업데이트
+      } catch (error) {
+        console.error('카테고리 데이터 가져오기 오류:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="my-5 flex min-w-375pxr max-w-680pxr flex-wrap justify-center gap-y-2">
-      {category.map(({ id, icon, collectionId, backgroundColor, keyword }) => (
-        <CategoryItem
-          key={id}
-          icon={`${import.meta.env.VITE_PB_URL}/api/files/${collectionId}/${id}/${icon}`}
-          backgroundColor={backgroundColor}
-          keyword={keyword}
-        />
-      ))}
+      {categories.map(
+        ({ id, icon, collectionId, backgroundColor, keyword }) => (
+          <CategoryItem
+            key={id}
+            icon={`${import.meta.env.VITE_PB_URL}/api/files/${collectionId}/${id}/${icon}`}
+            backgroundColor={backgroundColor}
+            keyword={keyword}
+          />
+        )
+      )}
     </div>
   );
 }
