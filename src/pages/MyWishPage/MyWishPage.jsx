3737e51b74ba18f9f3f0a9650cb701dffbc74419
bambiru <dj70083798@gmail.com>
2024-03-08 15:20:43 +0900

    [✨Feat]: TabBar에 Link 경로 추가
    Link로 연결해놨고, 일단 임시 페이지는 만들어놓은 상태입니다.

---
+++ src/pages/MyWishPage/MyWishPage.jsx
@@ -0,0 +1,12 @@
+import { TabBar } from '@/components/atoms';
+
+function MyWishPage() {
+  return (
+    <div>
+      <p>내 찜목록</p>
+      <TabBar />
+    </div>
+  );
+}
+
+export default MyWishPage;
---
+++ src/pages/ReviewPage/ReviewPage.jsx
@@ -0,0 +1,12 @@
+import { TabBar } from '@/components/atoms';
+
+function ReviewPage() {
+  return (
+    <>
+      <p>내리뷰?전체리뷰?</p>
+      <TabBar />
+    </>
+  );
+}
+
+export default ReviewPage;
