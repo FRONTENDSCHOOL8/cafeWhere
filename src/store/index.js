749c96ab1e7765ac349e34f6395677e50d44b2ca
bambiru <dj70083798@gmail.com>
2024-03-08 14:52:38 +0900

    [✨Feat]: 상세페이지 탭 관련 & 리뷰쪽 마크업
    - 상세페이지쪽 컴포넌트 합치기 진행중
    - 리뷰쪽 컴포넌트 합치기 (진행중)

--- src/store/index.js
+++ src/store/index.js
@@ -2,3 +2,4 @@ export { default as useTabStore } from './useTabStore';
 export { default as useCafeStore } from './useCafeStore';
 export { default as useRegionStore } from './useRegionStore';
 export { default as useCafeListStore } from './useCafeListStore';
+export { default as useActiveTabStore } from './useActiveTabStore';
