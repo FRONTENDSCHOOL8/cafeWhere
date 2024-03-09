37b7644ab388243541f7e69ccdac1c74f012044a
bambiru <dj70083798@gmail.com>
2024-03-09 23:00:37 +0900

    [✨Feat]: Hashtag 검색 결과 페이지 분리
    - Hashtag 검색 결과 페이지 분리 및 NoResult 약간의 코드 수정

--- src/pages/HashSearchPage/HashSearchPage.jsx
+++ src/pages/HashSearchPage/HashSearchPage.jsx
@@ -1,15 +1,16 @@
 import { HeaderBar, TabBar } from '@/components/atoms';
 import LoginButton from '@/components/atoms/LoginButton/LoginButton';
 import SearchBar from '@/components/atoms/Searchbar/Searchbar';
-import CafeListItem from '@/components/organisms/CafeListItem/CafeListItem';
 import { useHashtagStore } from '@/store';
 import pb from '@/utils/pocketbase';
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AllHashtagList } from '..';
 
 function HashSearchPage() {
-  const { hashtag } = useHashtagStore();
-  const [searchHashtag, setSearchHashtag] = useState({ items: [] });
+  const navigate = useNavigate();
+  const { hashtag, setHashtag } = useHashtagStore();
+  const { setSearchHashtag } = useHashtagStore();
   const handleSubmitHashtag = (e) => {
     e.preventDefault();
 
@@ -18,7 +19,13 @@ function HashSearchPage() {
       return;
     }
     fetchData();
+    navigate('/hash');
   };
+
+  useEffect(() => {
+    setHashtag([]);
+    setSearchHashtag({ items: [] });
+  }, []);
 
   const fetchData = async () => {
     let filterQuery = hashtag?.map((tag) => `hashtag~'${tag}'`).join(' && ');
@@ -38,9 +45,6 @@ function HashSearchPage() {
         <LoginButton>해시태그 검색</LoginButton>
       </form>
 
-      {searchHashtag.items.length > 0
-        ? searchHashtag.items.map((item) => <CafeListItem data={item} />)
-        : '없음'}
       <TabBar />
     </>
   );
--- src/store/useHashtagStore.js
+++ src/store/useHashtagStore.js
@@ -3,5 +3,8 @@ import { create } from 'zustand';
 const useHashtagStore = create((set) => ({
   hashtag: [],
   setHashtag: (newHashtag) => set({ hashtag: newHashtag }),
+  searchHashtag: { item: [] },
+  setSearchHashtag: (newSearchHashtag) =>
+    set(() => ({ searchHashtag: newSearchHashtag })),
 }));
 export default useHashtagStore;
