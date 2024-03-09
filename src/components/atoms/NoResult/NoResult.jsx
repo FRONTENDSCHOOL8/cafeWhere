03871d031b30a6d2dba60728bdd943ada11d59ea
mondimhaus <mondimhaus@naver.com>
2024-03-09 23:57:16 +0900

    [🎨Modify]: NoResult 컴포넌트 코드 수정
    1. 불필요한 클래스명을 삭제함

--- src/components/atoms/NoResult/NoResult.jsx
+++ src/components/atoms/NoResult/NoResult.jsx
@@ -1,6 +1,6 @@
 function NoResult() {
   return (
-    <div className="relative">
+    <div>
       <div className="flex h-screen flex-col items-center justify-center">
         <img src="/cup.png" alt="빈 컵" />
         <p className="">검색 결과가 없습니다.</p>
