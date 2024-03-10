201a31a6c1f3147c1796e5e2c6940a67f6813b51
junghyunlee0922 <self21c123@naver.com>
2024-03-10 21:59:20 +0900

    [✨Feat]: 상세페이지 리뷰 컴포넌트 데이터 넣기 및 해시태그 기능 추가 1. 리뷰 컴퍼논트에 데이터 넣기. 2. 해시태그 갯수 많아질때 +버튼 기능구현.

--- src/components/organisms/DetailReviewList/ReviewList.jsx
+++ src/components/organisms/DetailReviewList/ReviewList.jsx
@@ -1,97 +1,52 @@
-import Hashtag from '../../atoms/Hashtag/Hashtag';
+import CoffeeScore from '@/components/atoms/CoffeeScore/CoffeeScore';
+import Hashtag from '@/components/atoms/Hashtag/Hashtag';
+import { useEffect, useState } from 'react';
+import HashtagCard from './HashtagCard';
+import pb from '@/utils/pocketbase';
 
 function ReviewList() {
+  const [reviewData, setReviewData] = useState([]);
+
+  useEffect(() => {
+    handleCheck();
+  }, []);
+
+  const handleCheck = async () => {
+    const records = await pb.collection('review').getList(1, 50, {
+      sort: '-created',
+      expand: 'hashtag',
+    });
+
+    setReviewData(records.items);
+  };
+
   return (
-    <div className="mx-5 flex justify-center text-greyscale-80">
-      <article className="w-335pxr rounded-[15px] border border-greyscale-40 p-5">
-        <span className="mr-2 text-14pxr font-semibold text-additional-colors-dark">
-          닉네임 넣을 곳
-        </span>
-        <img
-          className="inline-block"
-          src="/images/review/property-0.5.svg"
-          alt="0.5점짜리 평점"
-        />
-        <p className="mb-1 mt-6pxr text-10pxr">24.03.06</p>
-        <p className="mb-2 mr-6 min-w-180pxr text-12pxr">
-          어머 여기 카페 너무 맛있고 분위기도 좋고 너무 좋아여!!! 어머 여기 카페
-          너무 맛있고 분위기도 좋고 너무 좋아여!!! 어머 여기 카페 너무 맛있고
-          분위기도 좋고 너무 좋아여!!!좋아여!!! 어머 여기 카페 너무 맛있고
-          분위기도 좋고 너무 좋아여!!!좋아여!!! 어머 여기 카페 너무 맛있고
-          분위기도 좋고 너무 좋아여!!!
-        </p>
-        <img
-          className="h-90pxr w-90pxr"
-          src="/images/review/test.png"
-          alt="김밤비!!! 사진"
-        />
-        <Hashtag icon={'☕️'} keyword={'커피가 맛있어요'} />
-      </article>
-
-      {/* 효진님이 주심 코드 이미지 옆에 */}
-      <article className="w-335pxr rounded-[15px] border border-greyscale-40 p-20pxr">
-        <span className="mr-2 text-14pxr font-semibold text-additional-colors-dark">
-          닉네임 넣을 곳
-        </span>
-        <img
-          className="inline-block"
-          src="/images/review/property-0.5.svg"
-          alt="0.5점짜리 평점"
-        />
-        <p className="mb-1 mt-6pxr text-10pxr">24.03.06</p>
-        <div className="flex">
-          <div>
-            <p className="mb-2 mr-6 min-h-54pxr min-w-180pxr text-12pxr">
-              어머 여기 카페 너무 맛있고 분위기도 좋고 너무 좋아여!!! 어머 여기
-              카페 너무 맛있고 분위기도 좋고 너무 좋아여!!! 어머 여기 카페 너무
-              맛있고 분위기도 좋고 너무 좋아여!!! 어머 여기 카페 너무 맛있고
-              분도 좋고 너무 좋아여!!! 어머 여기 카페 너무 맛있고 분도 좋고 너무
-              좋아여!!! 어머 여기 카페 너무 맛있고 분도 좋고 너무 좋아여!!! 어머
-              여기 카페 너무 맛있고 분도 좋고 너무 좋아여!!! 어머 여기 카페 너무
-              맛있고 분
-            </p>
-            <Hashtag icon={'☕️'} keyword={'커피가 맛있어요'} />
+    <>
+      {reviewData.map((data) => {
+        return (
+          <div key={data.id} className="mx-5 flex justify-center">
+            <article className="w-335pxr rounded-[15px] border border-greyscale-40 p-5">
+              <div className="flex items-center">
+                <span className="mr-5pxr text-15pxr font-semibold text-additional-colors-dark">
+                  {data.userId}
+                </span>
+                <CoffeeScore score={data.score} />
+              </div>
+              <p className="mb-1 mt-1pxr text-12pxr text-greyscale-60">{}</p>
+              <div className="mb-3 grid grid-cols-3 gap-2pxr">
+                <img src="/images/review/test.png" alt="김밤비!!! 사진" />
+                <img src="/images/review/test.png" alt="김밤비!!! 사진" />
+                <img src="/images/review/test.png" alt="김밤비!!! 사진" />
+              </div>
+              <p className="mb-2 text-13pxr text-greyscale-80">
+                {data.contents}
+              </p>
+              <HashtagCard review={data} />
+            </article>
           </div>
-          <img
-            className="h-90pxr w-90pxr"
-            src="/images/review/test.png"
-            alt="김밤비!!! 사진"
-          />
-        </div>
-      </article>
-
-      {/* 효진님이 주신 코드 이미지 아래 */}
-      <article className="w-335pxr rounded-[15px] border border-greyscale-40 p-20pxr">
-        <span className="mr-2 text-14pxr font-semibold text-additional-colors-dark">
-          닉네임 넣을 곳
-        </span>
-        <img
-          className="inline-block"
-          src="/images/review/property-0.5.svg"
-          alt="0.5점짜리 평점"
-        />
-        <p className="mb-1 mt-6pxr text-10pxr">24.03.06</p>
-        <div className="flex">
-          <div>
-            <p className="mb-2 mr-6 min-h-54pxr min-w-180pxr text-12pxr">
-              어머 여기 카페 너무 맛있고 분위기도 좋고 너무 좋아여!!! 어머 여기
-              카페 너무 맛있고 분위기도 좋고 너무 좋아여!!! 어머 여기 카페 너무
-              맛있고 분위기도 좋고 너무 좋아여!!! 어머 여기 카페 너무 맛있고
-              분도 좋고 너무 좋아여!!! 어머 여기 카페 너무 맛있고 분도 좋고 너무
-              좋아여!!! 어머 여기 카페 너무 맛있고 분도 좋고 너무 좋아여!!! 어머
-              여기 카페 너무 맛있고 분도 좋고 너무 좋아여!!! 어머 여기 카페 너무
-              맛있고 분
-            </p>
-            <Hashtag icon={'☕️'} keyword={'커피가 맛있어요'} />
-            <img
-              className="h-90pxr w-90pxr"
-              src="/images/review/test.png"
-              alt="김밤비!!! 사진"
-            />
-          </div>
-        </div>
-      </article>
-    </div>
+        );
+      })}
+    </>
   );
 }
 
