44ccb2b5054878c3a273209d524ff41d98fba102
bambiru <dj70083798@gmail.com>
2024-03-10 18:55:00 +0900

    [💄Design]: TabBar 색상 수정
    TabBar 배경 및 텍스트/아이콘 색상 수정

--- src/components/atoms/TabBar/TabBar.jsx
+++ src/components/atoms/TabBar/TabBar.jsx
@@ -6,67 +6,65 @@ function TabBar() {
   const { activeTabState, setHome, setCafe, setReview, setWish, setMyicon } =
     useTabStore();
   return (
-    <div className="flex justify-center">
-      <div className="fixed bottom-0 z-50 flex h-88pxr w-full min-w-375pxr max-w-680pxr items-center justify-around rounded-t-[32px] bg-black shadow-lg">
-        <div onClick={setHome}>
-          <Link to="/main">
-            <img
-              src={
-                activeTabState === 'home'
-                  ? '/images/main/tabbar/home-r.svg'
-                  : '/images/main/tabbar/home-b.svg'
-              }
-            />
-          </Link>
-        </div>
+    <div className="fixed bottom-0 z-50 flex h-88pxr w-full min-w-375pxr max-w-680pxr items-center justify-around rounded-t-[32px]  bg-stone-700 shadow-lg">
+      <div onClick={setHome}>
+        <Link to="/main">
+          <img
+            src={
+              activeTabState === 'home'
+                ? '/images/main/tabbar/home-r.svg'
+                : '/images/main/tabbar/home-b.svg'
+            }
+          />
+        </Link>
+      </div>
 
-        <div onClick={setCafe}>
-          <Link to="/hashSearch">
-            <img
-              src={
-                activeTabState === 'cafeicon'
-                  ? '/images/main/tabbar/cafeicon-r.svg'
-                  : '/images/main/tabbar/cafeicon-b.svg'
-              }
-            />
-          </Link>
-        </div>
+      <div onClick={setCafe}>
+        <Link to="/hashSearch">
+          <img
+            src={
+              activeTabState === 'cafeicon'
+                ? '/images/main/tabbar/cafeicon-r.svg'
+                : '/images/main/tabbar/cafeicon-b.svg'
+            }
+          />
+        </Link>
+      </div>
 
-        <div onClick={setReview}>
-          <Link to="/review">
-            <img
-              src={
-                activeTabState === 'review'
-                  ? '/images/main/tabbar/review-r.svg'
-                  : '/images/main/tabbar/review-b.svg'
-              }
-            />
-          </Link>
-        </div>
+      <div onClick={setReview}>
+        <Link to="/myreview">
+          <img
+            src={
+              activeTabState === 'review'
+                ? '/images/main/tabbar/review-r.svg'
+                : '/images/main/tabbar/review-b.svg'
+            }
+          />
+        </Link>
+      </div>
 
-        <div onClick={setWish}>
-          <Link to="/myWish">
-            <img
-              src={
-                activeTabState === 'wish'
-                  ? '/images/main/tabbar/wish-r.svg'
-                  : '/images/main/tabbar/wish-b.svg'
-              }
-            />
-          </Link>
-        </div>
+      <div onClick={setWish}>
+        <Link to="/myWish">
+          <img
+            src={
+              activeTabState === 'wish'
+                ? '/images/main/tabbar/wish-r.svg'
+                : '/images/main/tabbar/wish-b.svg'
+            }
+          />
+        </Link>
+      </div>
 
-        <div onClick={setMyicon}>
-          <Link to="/mypage">
-            <img
-              src={
-                activeTabState === 'myicon'
-                  ? '/images/main/tabbar/myicon-r.svg'
-                  : '/images/main/tabbar/myicon-b.svg'
-              }
-            />
-          </Link>
-        </div>
+      <div onClick={setMyicon}>
+        <Link to="/mypage">
+          <img
+            src={
+              activeTabState === 'myicon'
+                ? '/images/main/tabbar/myicon-r.svg'
+                : '/images/main/tabbar/myicon-b.svg'
+            }
+          />
+        </Link>
       </div>
     </div>
   );
