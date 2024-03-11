71a858455ce52a97bcd0c696961be1e3f4e6b15b
bambiru <dj70083798@gmail.com>
2024-03-11 14:36:14 +0900

    [♻️Refactor]: 지역검색부분 나타내기

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,13 +4,16 @@ import SwiperCafeList from '@/components/SwiperCafeList/SwiperCafeList';
 import { MainHeader, TabBar } from '@/components/atoms';
 import CafeListItem from '@/components/organisms/CafeListItem/CafeListItem';
 import Category from '@/components/organisms/Category/Category';
-import { useRegionStore } from '@/store';
+import { useCafeListStore, useRegionStore } from '@/store';
 import pb from '@/utils/pocketbase';
 import { useQuery } from '@tanstack/react-query';
 import Footer from './../../components/atoms/Footer/Footer';
+import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 function MainPage() {
   const { region } = useRegionStore();
+  const { setCafeList } = useCafeListStore();
 
   const { data: cafeData } = useQuery({
     queryKey: ['cafeData', region],
@@ -19,8 +22,13 @@ function MainPage() {
         filter: `address~'${region}'`,
         sort: '-created',
       }),
+
     enabled: !!region,
   });
+
+  useEffect(() => {
+    setCafeList(cafeData);
+  }, [region, cafeData]);
 
   return (
     <>
--- src/pages/SearchRegionList/SearchRegionList.jsx
+++ src/pages/SearchRegionList/SearchRegionList.jsx
@@ -21,15 +21,12 @@ function SearchRegionList() {
     };
     fetchData();
 
-    // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
-
-    const mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    const mapContainer = document.getElementById('map'),
       mapOption = {
-        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
+        center: new kakao.maps.LatLng(37.566826, 126.9786567),
+        level: 3,
       };
 
-    // 지도를 생성합니다
     const map = new kakao.maps.Map(mapContainer, mapOption);
 
     // 장소 검색 객체를 생성합니다
