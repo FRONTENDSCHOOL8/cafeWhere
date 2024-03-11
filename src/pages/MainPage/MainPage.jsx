ebb1937f8a2316e98f9bac6d2ba03abb308d2e81
bambiru <dj70083798@gmail.com>
2024-03-11 13:49:51 +0900

    [♻️Refactor]: 메인로고 이미지 폰트 변경 및 MainHeader css변경

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -7,17 +7,12 @@ import Category from '@/components/organisms/Category/Category';
 import { useRegionStore } from '@/store';
 import pb from '@/utils/pocketbase';
 import { useQuery } from '@tanstack/react-query';
-import { useEffect } from 'react';
 import Footer from './../../components/atoms/Footer/Footer';
 
 function MainPage() {
   const { region } = useRegionStore();
 
-  const {
-    data: cafeData,
-    isLoading,
-    error,
-  } = useQuery({
+  const { data: cafeData } = useQuery({
     queryKey: ['cafeData', region],
     queryFn: async () =>
       await pb.collection('cafe').getFullList({
@@ -27,37 +22,33 @@ function MainPage() {
     enabled: !!region,
   });
 
-  useEffect(() => {}, [region]);
-
   return (
-    <div>
-      <div className="h-full">
-        <div className="mx-auto w-full min-w-375pxr max-w-680pxr pb-8">
-          <MainHeader />
-          <HeaderSwiper />
-          <Category />
-          <div className="mt-12">
-            <CategoryListName>노트북하기 최적화?!</CategoryListName>
-            <SwiperCafeList data={cafeData} />
-          </div>
-          <div className="mt-12">
-            <CategoryListName>너도 나도 인스타 업로드!!</CategoryListName>
-            <SwiperCafeList data={cafeData} />
-          </div>
-          <div className="mt-12">
-            <CategoryListName>전체보기</CategoryListName>
-            <div className="flex flex-col gap-4">
-              {cafeData &&
-                cafeData.map((data) => (
-                  <CafeListItem key={data.id} data={data} />
-                ))}
-            </div>
+    <>
+      <div className="pb-8">
+        <MainHeader />
+        <HeaderSwiper />
+        <Category />
+        <div className="mt-12">
+          <CategoryListName>노트북하기 최적화?!</CategoryListName>
+          <SwiperCafeList data={cafeData} />
+        </div>
+        <div className="mt-12">
+          <CategoryListName>너도 나도 인스타 업로드!!</CategoryListName>
+          <SwiperCafeList data={cafeData} />
+        </div>
+        <div className="mt-12">
+          <CategoryListName>전체보기</CategoryListName>
+          <div className="flex flex-col gap-4">
+            {cafeData &&
+              cafeData.map((data) => (
+                <CafeListItem key={data.id} data={data} />
+              ))}
           </div>
         </div>
-        <Footer />
-        <TabBar />
       </div>
-    </div>
+      <Footer />
+      <TabBar />
+    </>
   );
 }
 
