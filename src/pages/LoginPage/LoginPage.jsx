3c8fe1f287e9a9f343ad5cdeabaa9276461497ac
mondimhaus <mondimhaus@naver.com>
2024-03-11 10:32:47 +0900

    [🚚Rename]: SIgnUpLink 경로 수정 1. LoginPage, SelectLoginPage의 SIgnUpLink 경로를 수정

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { HeaderBar } from '@/components/atoms';
 import Divider from '@/components/atoms/Divider/Divider';
 import LoginButton from '@/components/atoms/LoginButton/LoginButton';
-import SignUpLink from '@/components/atoms/SignUpLink/SignUpLink';
+import SignUpLink from '@/components/atoms/SIgnUpLink/SignUpLink';
 import SocialLoginButton from '@/components/atoms/SocialLoginButton/SocialLoginButton';
 import { LoginInput } from '@/components/organisms';
 
