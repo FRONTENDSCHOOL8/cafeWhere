d14ab8286a3f4dcdc59ae43c3c896fc6520b9b7e
JungHyun.Lee <148926098+junghyunlee0922@users.noreply.github.com>
2024-03-12 15:57:28 +0900

    [✨Feat]: 메인페이지 컴포리뷰 별점 해시태그에 데이터 넣기 (#71)

--- src/components/SwiperCafeList/CategoryCafeList.jsx
+++ src/components/SwiperCafeList/CategoryCafeList.jsx
@@ -1,26 +1,39 @@
 import { Wish } from '@/components/atoms';
 import Hashtag from '../atoms/Hashtag/Hashtag';
 import { pbImg } from '@/utils/pocketbase';
+import { Link } from 'react-router-dom';
 
-// * 추후에 사진크기 조절
 function CategoryCafeList({ data }) {
   return (
     <div className="ml-5 h-285pxr w-200pxr rounded-[12px] border border-greyscale-50 bg-additional-colors-white p-4 font-medium leading-[100%]">
       <div className="relative">
         <img
+          className="h-140pxr w-168pxr rounded-xl"
           src={`${pbImg(data.collectionId, data.id, data.mainImage)}`}
-          alt="현재 카페 상세페이지"
+          alt="현재 카페 리스트"
         />
         <div className="absolute bottom-2 right-2">
           <Wish />
         </div>
       </div>
-      <span className="mb-2 mt-4 inline-block">{data.cafeName}</span>
-      <div className="flex text-10pxr">
-        <span className="mr-2">☕️ {data.score}</span>
-        <span>리뷰 {data.reviewQuantity}</span>
-      </div>
-      <Hashtag icon={'🍰'} keyword={'디저트가 맛있어요'} />
+      <Link to={`/detail/${data.id}`} className="w-full">
+        <div>
+          <span className="mb-2 mt-4 inline-block">{data.cafeName}</span>
+          <div className="mb-6pxr flex text-10pxr">
+            <span className="mr-2">☕️ {data.score}</span>
+            <span>리뷰 {data.reviewQuantity}</span>
+          </div>
+          {data.expand
+            ? data.expand.hashtag.map((item) => (
+                <Hashtag
+                  key={item.id}
+                  icon={item.icon}
+                  keyword={item.keyword}
+                />
+              ))
+            : ''}
+        </div>
+      </Link>
     </div>
   );
 }
