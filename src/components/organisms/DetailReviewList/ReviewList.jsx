4ed1e0bb3467127506cc4a03a3fb5d9306074c91
bambiru <dj70083798@gmail.com>
2024-03-13 10:02:41 +0900

    [♻️Refactor]: ReviewList 해당 카페에서 닉네임 보이게 수정
    DetailReview에서 해당 카페에 등록된 카페리뷰가 보이게 수정

--- src/components/organisms/DetailReviewList/ReviewList.jsx
+++ src/components/organisms/DetailReviewList/ReviewList.jsx
@@ -3,11 +3,11 @@ import Hashtag from '@/components/atoms/Hashtag/Hashtag';
 import { useEffect, useState } from 'react';
 import HashtagCard from './HashtagCard';
 import pb, { pbImg } from '@/utils/pocketbase';
+import { useCafeStore } from '@/store';
 
 function ReviewList() {
   const [reviewData, setReviewData] = useState([]);
-
-  const UserId = JSON.parse(localStorage.getItem('pocketbase_auth')).model.id;
+  const { cafe } = useCafeStore();
 
   useEffect(() => {
     handleCheck();
@@ -16,51 +16,45 @@ function ReviewList() {
   const handleCheck = async () => {
     const records = await pb.collection('review').getList(1, 5, {
       sort: '-created',
-      expand: 'hashtag',
-      filter: `email="${UserId}"`,
+      expand: 'hashtag, users',
+      filter: `cafeName="${cafe.cafeName}"`,
     });
 
     setReviewData(records.items);
   };
 
   return (
-    <>
-      {reviewData.map((data) => {
-        return (
-          <div key={data.id} className="mx-5 mb-10 ">
-            <article className="min-w-335pxr rounded-[15px] border border-greyscale-40 p-5">
-              <div className="flex items-center">
-                <span className="mr-5pxr text-15pxr font-semibold text-additional-colors-dark">
-                  {data.cafeName}
-                </span>
-                <CoffeeScore score={data.score} />
-              </div>
-              <p className="mb-1 mt-1pxr text-12pxr text-greyscale-60">
-                {data.updated.slice(0, 10)}
-              </p>
-              {/* <div className="mb-3 grid grid-cols-3 gap-2pxr">
-                <img src="/images/review/test.png" alt="김밤비!!! 사진" />
-                <img src="/images/review/test.png" alt="김밤비!!! 사진" />
-                <img src="/images/review/test.png" alt="김밤비!!! 사진" />
-              </div> */}
-              <div className="mb-3 grid auto-cols-max grid-flow-col gap-2pxr">
-                {data.image.map((img) => (
-                  <img
-                    className="h-90pxr w-90pxr"
-                    src={pbImg(data.collectionId, data.id, img)}
-                    alt={data.cafeName} //변경해야됨
-                  />
-                ))}
-              </div>
-              <p className="mb-2 text-13pxr text-greyscale-80">
-                {data.contents}
-              </p>
-              <HashtagCard review={data} />
-            </article>
+    <div className="mx-5 flex flex-col gap-2">
+      {reviewData.map((data) => (
+        <article
+          key={data.id}
+          className="min-w-335pxr rounded-[15px] border border-greyscale-40 p-5"
+        >
+          <div className="flex items-center">
+            <span className="mr-5pxr text-15pxr font-semibold text-additional-colors-dark">
+              {data.expand.users.nickname}
+            </span>
+            <CoffeeScore score={data.score} />
           </div>
-        );
-      })}
-    </>
+          <p className="mb-1 mt-1pxr text-12pxr text-greyscale-60">
+            {data.updated.slice(0, 10)}
+          </p>
+
+          <div className="mb-3 grid auto-cols-max grid-flow-col gap-2pxr">
+            {data.image.map((img) => (
+              <img
+                key={img}
+                className="h-90pxr w-90pxr"
+                src={pbImg(data.collectionId, data.id, img)}
+                alt={data.cafeName}
+              />
+            ))}
+          </div>
+          <p className="mb-2 text-13pxr text-greyscale-80">{data.contents}</p>
+          <HashtagCard review={data} />
+        </article>
+      ))}
+    </div>
   );
 }
 
