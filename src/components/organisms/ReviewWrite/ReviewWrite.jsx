c2389bd830ab7a022401f423f03fd7ea1947112c
bambiru <dj70083798@gmail.com>
2024-03-14 00:32:49 +0900

    [✨Feat]: 리뷰작성 시 리뷰갯수 및 평점 조절

--- src/components/organisms/ReviewWrite/ReviewWrite.jsx
+++ src/components/organisms/ReviewWrite/ReviewWrite.jsx
@@ -1,43 +1,26 @@
 import RatingScore from '@/components/atoms/RatingScore/RatingScore';
-import { AllHashtagList } from '@/pages';
-import { useHashtagStore } from '@/store';
-import { useUserIdStore } from '@/store/useLoginStore';
+import { useCafeStore, useHashtagStore } from '@/store';
 import pb from '@/utils/pocketbase';
-import React from 'react';
-import { useRef } from 'react';
-import { useState } from 'react';
-import { useCafeStore } from '@/store';
+import { useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 function ReviewWrite() {
   const reviewRef = useRef(null);
   const [rating, setRating] = useState(1);
-
-  const UserId = JSON.parse(localStorage.getItem('pocketbase_auth')).model.id;
-
-  const [reviewText, setReviewText] = useState(''); // 초기값으로 0을 설정합니다.
-  const [imagePreview, setImagePreview] = useState(null); // 리뷰 작성 텍스트 영역의 이미지 미리보기
-
+  const userId = JSON.parse(localStorage.getItem('pocketbase_auth')).model.id;
+  const [reviewText, setReviewText] = useState('');
+  const [imagePreview, setImagePreview] = useState(null);
   const { hashtag, resetHashtag } = useHashtagStore();
-
+  const params = useParams();
   const { cafe } = useCafeStore();
-
   const navigate = useNavigate();
   const handleReviewCheck = async (e) => {
     e.preventDefault();
 
     const formData = new FormData(e.target);
-
-    // const data = {
-    //   cafeName: cafe.cafeName,
-    //   email: UserId,
-    //   contents: reviewText,
-    //   hashtag: hashtag,
-    //   score: rating,
-    // };
-
     formData.append('cafeName', cafe.cafeName);
-    formData.append('users', UserId);
+    formData.append('users', userId);
     formData.append('contents', reviewText);
     formData.append('score', rating);
 
@@ -45,7 +28,15 @@ function ReviewWrite() {
       formData.append('hashtag', tag);
     });
 
-    const data = Object.fromEntries(formData.entries());
+    // const data = Object.fromEntries(formData.entries());
+
+    const data = {
+      ...cafe,
+      score: (cafe.score + rating) / cafe.reviewQuantity,
+      reviewQuantity: cafe.reviewQuantity + 1,
+    };
+
+    await pb.collection('cafe').update(params.id, data);
 
     await pb
       .collection('review')
@@ -54,16 +45,13 @@ function ReviewWrite() {
         alert('전송 완료');
         resetHashtag();
 
-        navigate('/');
-        // history.back(-3);
+        navigate(`/detail/${params.id}`);
       })
       .catch((err) => {
         console.log(err);
         console.log(hashtag);
       });
   };
-
-  const [imageList, setImageList] = useState([]);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -74,43 +62,41 @@ function ReviewWrite() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-8 rounded-xl">
-      <div className="my-200pxr w-500pxr ">
-        <form
-          onSubmit={handleReviewCheck}
-          className="flex flex-col items-center gap-4 rounded-lg bg-white py-4"
-          encType="multipart/form-data"
+    <div className="h-full min-h-svh">
+      <form
+        onSubmit={handleReviewCheck}
+        className="flex flex-col items-center gap-4 rounded-lg bg-white py-4"
+        encType="multipart/form-data"
+      >
+        <span className="text-lg font-semibold">리뷰 쓰기</span>
+        <RatingScore rating={rating} setRating={setRating} />
+        <textarea
+          placeholder="리뷰를 남겨주세요"
+          ref={reviewRef}
+          className="h-300pxr w-300pxr overflow-auto rounded-md bg-slate-100"
+          onChange={(e) => setReviewText(e.target.value)}
+        ></textarea>
+
+        <div className="flex flex-col items-center">
+          <input
+            type="file"
+            accept="*.jpg,*.png,*.jpeg,*.webp,*.avif,*.svg"
+            multiple
+            id="photos"
+            name="image"
+            onChange={handleImageChange}
+          />
+
+          {imagePreview && <img src={imagePreview} alt="테스트 이미지" />}
+        </div>
+
+        <button
+          type="submit"
+          className="w-300pxr rounded-md bg-black py-4 text-white"
         >
-          <span className="text-lg font-semibold">리뷰 쓰기</span>
-          <RatingScore rating={rating} setRating={setRating} />
-          <textarea
-            placeholder="리뷰를 남겨주세요"
-            ref={reviewRef}
-            className="h-300pxr w-300pxr overflow-auto rounded-md bg-slate-100"
-            onChange={(e) => setReviewText(e.target.value)}
-          ></textarea>
-
-          <div className="flex flex-col items-center">
-            <input
-              type="file"
-              accept="*.jpg,*.png,*.jpeg,*.webp,*.avif,*.svg"
-              multiple
-              id="photos"
-              name="image"
-              onChange={handleImageChange}
-            />
-
-            {imagePreview && <img src={imagePreview} alt="테스트 이미지" />}
-          </div>
-
-          <button
-            type="submit"
-            className="w-300pxr rounded-md bg-black py-4 text-white"
-          >
-            작성하기
-          </button>
-        </form>
-      </div>
+          작성하기
+        </button>
+      </form>
     </div>
   );
 }
