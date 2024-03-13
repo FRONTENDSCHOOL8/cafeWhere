95bb69d7c6690d6a24be3d205ac330eaf6ba2d37
bambiru <dj70083798@gmail.com>
2024-03-13 10:19:17 +0900

    [♻️Refactor]: 스와이퍼 위치 조절

--- src/components/HeaderSwiper/HeaderSwiper.jsx
+++ src/components/HeaderSwiper/HeaderSwiper.jsx
@@ -8,8 +8,10 @@ import 'swiper/css/pagination';
 
 function HeaderSwiper() {
   const [swiperIndex, setSwiperIndex] = useState(0);
-  const [swiper, setSwiper] = useState(); // 슬라이드용
+  const [swiper, setSwiper] = useState();
 
+  const slideButtonStyle =
+    'absolute top-[50%] translate-y-[-50%] z-10 rounded-full bg-slate-200/60';
   return (
     <div className="relative">
       <Swiper
@@ -41,13 +43,13 @@ function HeaderSwiper() {
           <img src="/images/main/swiper/swiperBigImg3.svg" alt="" />
         </SwiperSlide>
       </Swiper>
-      <div className="prev-slide-button absolute left-20pxr top-125pxr z-10 rounded-full bg-slate-200/60">
+      <div className={`prev-slide-button left-20pxr ${slideButtonStyle}`}>
         <img src="/images/main/swiper/leftArrow.svg" alt="" />
       </div>
-      <div className="next-slide-button absolute right-20pxr top-125pxr z-10 rounded-full bg-slate-200/60">
+      <div className={`next-slide-button right-20pxr ${slideButtonStyle}`}>
         <img src="/images/main/swiper/rightArrow.svg" alt="" />
       </div>
-      <div className=" absolute bottom-3 right-20pxr z-10 flex h-20pxr w-50pxr items-center justify-center rounded-xl bg-gray-500/60 text-center text-white">
+      <div className="absolute bottom-3 right-20pxr z-10 flex h-20pxr w-50pxr items-center justify-center rounded-xl bg-gray-500/60 text-center text-white">
         <span>{swiperIndex + 1}</span>
         <span>{'/'}</span>
         <span>{3}</span>
