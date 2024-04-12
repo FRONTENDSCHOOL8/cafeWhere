16d65e3abbe79b72edef3f52ab4a7f9321a2c097
bambiru <dj70083798@gmail.com>
2024-04-12 14:02:59 +0900

    [🐛Fix]: CafeReviewInfo 수정
    - 데이터가 없을 경우 나타나는 버그 수정

--- src/components/atoms/CafeReviewInfo/CafeReviewInfo.jsx
+++ src/components/atoms/CafeReviewInfo/CafeReviewInfo.jsx
@@ -1,7 +1,7 @@
 function CafeReviewInfo({ data }) {
   return (
     <p className="my-2 flex gap-2 text-13pxr">
-      <span>☕ {data.score.toFixed(1)}</span>
+      <span>☕ {data?.score?.toFixed(1)}</span>
       <span>리뷰 {data.reviewQuantity}</span>
     </p>
   );
